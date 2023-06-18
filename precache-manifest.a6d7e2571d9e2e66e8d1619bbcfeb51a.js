8cab9baed675cdd54f2bf1426826efa1d231262e
borashek <borashek@inbox.ru>
2023-06-18 12:41:40 +0300

    Updates

---
+++ precache-manifest.a6d7e2571d9e2e66e8d1619bbcfeb51a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5812d94bd852548507fc50586b5965ca",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "9c849aac39977c9701f3",
+    "url": "/todo/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "74b26f97c108942f9a97",
+    "url": "/todo/static/js/2.abf1ffd7.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo/static/js/2.abf1ffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c849aac39977c9701f3",
+    "url": "/todo/static/js/main.b1889638.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
